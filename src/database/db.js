8cdcbe1b9d790896fa0394ff1055ca899ee17a7d
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-23 21:30:36 -0300

    config: MongoClient and collections

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = MongoClient(process.env.MOGO_URI);
+
+try {
+    await mongoClient.connect();
+    console.log("MongoDB Connected!");
+} catch (error) {
+    console.log(error);
+}
+
+const db = mongoClient.db("ChessPush");
+export const userCollection = db.collection("users");
+export const sessionsCollections = db.collection("sessions");
+export const productsCollections = db.collection("products");
+export const orderedsCollections = db.collection("ordereds");
