44c997a20e8f3dadfdab21f765b7875268d2e028
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-11-25 20:55:03 -0300

    refact: alteração products.controlers

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(router)
+app.use(router);
 
-db
+db;
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port ${port}`));
