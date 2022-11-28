384da39d71c23601d9c8aaffd06e78b37f48dd59
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-27 23:40:59 -0300

    fix: userSchema CPF

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,6 +3,7 @@ import joi from "joi"
 export const userSchema = joi.object({
     name: joi.string().required().min(3),
     email: joi.string().email().required().min(6),
+    cpf: joi.string().required().min(11).max(11),
     password: joi.string().required().min(3)
 });
 
