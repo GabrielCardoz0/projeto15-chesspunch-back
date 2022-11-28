c1c63f959c988365cab63ca443915db3bc6bb4ea
rafaelBporto <rafaelbporto88@gmail.com>
2022-11-28 17:29:11 -0300

    feat: order pix and boleto

--- src/middlewares/oderSchemaValidation.middleware.js
+++ src/middlewares/oderSchemaValidation.middleware.js
@@ -1,5 +1,5 @@
 import { orderedsCollections, sessionsCollections } from "../database/db.js";
-import { creditCardSchema } from "../models/order.model.js";
+import { creditCardSchema, orderSchema } from "../models/order.model.js";
 
 export async function orderSchemaValidation(req, res, next) {
     const order = req.body;
@@ -11,20 +11,28 @@ export async function orderSchemaValidation(req, res, next) {
             const errors = error.details.map((detail) => detail.message);
             res.status(400).send(errors);
         }
+    }
 
-        const token = authorization?.replace("Bearer ", "");
-        try {
-            const session = await sessionsCollections.findOne({ token });
-            if( !session){
-                return res.sendStatus(401);
-            }
-            res.locals.userId = session.userId;
-            res.locals.order = order;
-            
-        } catch (error){
-            console.log(error);
-            res.sendStatus(500)
+    if (req.body.method === "pix" || req.body.method === "boleto") {
+        const { error } = orderSchema.validate(order, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((detail) => detail.message);
+            res.status(400).send(errors);
         }
+    }
+
+    const token = authorization?.replace("Bearer ", "");
+    try {
+        const session = await sessionsCollections.findOne({ token });
+        if (!session) {
+            return res.sendStatus(401);
+        }
+        res.locals.userId = session.userId;
+        res.locals.order = order;
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500)
     }
 
     next();
